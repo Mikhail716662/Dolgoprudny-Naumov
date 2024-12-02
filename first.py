ad28841e4fe324a628feb2ab591d4bfc95b2c273
Mikhail <numizmataleks@gmail.com>
2024-12-02 18:35:36 +0300

    изменил очень в. комент(Наумов)

--- first.py
+++ first.py
@@ -1,1 +1,10 @@
-print("My first git-repo")
+def main():
+    print('My first git program')
+    print('And I change it every day')
+    # изменил очень в. комент
+    print('Again')
+    print('UFO came and added this line')
+
+
+if __name__ == '__main__':
+    main()

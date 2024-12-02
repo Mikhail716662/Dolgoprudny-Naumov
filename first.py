2f4b4d062f34f154f68e48e132cb5ea2d18931aa
Mikhail <numizmataleks@gmail.com>
2024-12-02 18:52:40 +0300

    получил

--- first.py
+++ first.py
@@ -1,1 +1,16 @@
-print("My first git-repo")
+def super_function():
+    # я наумов
+    print("ха ха ха")
+    print('отстань')
+    print('получил')
+
+
+def main():
+    print('My first git program')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
+
+
+if __name__ == '__main__':
+    main()

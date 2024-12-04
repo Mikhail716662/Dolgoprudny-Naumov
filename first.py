4b1122258d6cfd7f1fce696296e1aca7a529761c
Mikhail716662 <numizmataleks@gmail.com>
2024-12-04 16:18:52 +0300

    Update first.py
    git-repo

--- first.py
+++ first.py
@@ -1,17 +1,1 @@
-def super_function():
-    # я наумов
-    print("ха ха ха")
-    print('отстань')
-    print('получил')
-
-
-def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('Again')
-    print('UFO came and added this line')
-    print('прости, я ухожу')
-
-
-if __name__ == '__main__':
-    main()
+print("My first git-repo")
